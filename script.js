d31c881ed3490ee192d6a2784f9ff31f6f184c32
Praveen Kumar <praveenscience@gmail.com>
2020-10-08 17:06:12 +0100

    Role rotation added animation.

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ $(function () {
   var roles = ["Student", "VITian", "Artist"];
   var count = 0;
   setInterval(() => {
-    $("#intro .content h2 span").text(roles[count++ % roles.length]);
+    $("#intro .content h2 span").fadeOut("fast", function () {
+      $(this).text(roles[count++ % roles.length]);
+      $(this).fadeIn("fast");
+    });
   }, 1000);
 });
