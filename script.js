e89d7bcf52908be60ef923d70a884a93b8784341
Praveen Kumar <praveenscience@gmail.com>
2020-10-09 16:30:56 +0100

    Implemented typewriter effect.

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // Wait till the DOM is ready.
 $(function () {
-  var roles = ["Student", "VITian", "Artist"];
+  var roles = ["Artist", "Student", "VITian"];
   var count = 0;
+  var $typeSpan = $("#intro .content h2 span").typist({
+    text: roles[count]
+  });
   setInterval(() => {
-    $("#intro .content h2 span").fadeOut("fast", function () {
-      $(this).text(roles[count++ % roles.length]);
-      $(this).fadeIn("fast");
-    });
-  }, 1000);
+    $typeSpan
+      .typistRemove(roles[count++ % roles.length].length)
+      .typistPause(2000)
+      .typistAdd(roles[count % roles.length]);
+  }, 5000);
   $(".navbar").hide();
   $(window).scroll(function () {
     if ($(window).scrollTop() > 40) {
