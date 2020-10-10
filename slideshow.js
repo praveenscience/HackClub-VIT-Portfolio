c3775d27a9fe0017935a7f52a09b299a472cf5fc
Praveen Kumar <praveenscience@gmail.com>
2020-10-10 17:11:17 +0100

    Added bulleted pager.

--- slideshow.js
+++ slideshow.js
@@ -1,10 +1,15 @@
 $(function () {
   $(".hidden").hide().removeClass("hidden");
+  $(".pager").append(new Array($(".slides .slide").length + 1).join("<li></li>"));
   $(".slides .slide").first().show();
+  $(".pager li").first().addClass("active");
   var counter = 1;
   setInterval(() => {
     $(".slides .slide:visible").fadeOut(function () {
-      $(".slides .slide:nth-child(" + ((counter++ % $(".slides .slide").length) + 1) + ")").fadeIn();
+      curSlide = (counter++ % $(".slides .slide").length) + 1;
+      $(".slides .slide:nth-child(" + curSlide + ")").fadeIn();
+      $(".pager li.active").removeClass("active");
+      $(".pager li:nth-child(" + curSlide + ")").addClass("active");
     });
   }, 2500);
 });
